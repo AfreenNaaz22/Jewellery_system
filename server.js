9d2532d3ea2a389663b2ad3a5801b0ce99868b85
Afreen Naaz <148052690+AfreenNaaz22@users.noreply.github.com>
2025-11-15 02:43:36 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+const productsRouter = require('./routes/products');
+const usersRouter = require('./routes/users');
+const cartRouter = require('./routes/cart');
+const ordersRouter = require('./routes/orders');
+const wishlistRouter = require('./routes/wishlist');
+
+app.use('/api/products', productsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/orders', ordersRouter);
+app.use('/api/wishlist', wishlistRouter);
+
+// Serve index.html for root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
